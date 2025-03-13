929348699b665b0db9efb8bb34bfa109d6af64eb
jesus <jesuselvirasanchez@gmail.com>
2025-03-13 07:59:02 +0100

    Creado el microservicio gestión de operaciones.

---
+++ src/test/java/com/microservicioOperaciones/microservicioOperaciones/MicroservicioOperacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicioOperaciones.microservicioOperaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioOperacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
